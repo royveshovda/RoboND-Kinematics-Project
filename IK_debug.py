da0fcfe6724a2cc66d8684561e8308fce7d3635f
Roy Veshovda <roy@veshovda.no>
2017-07-28 10:58:10 -0700

    feat: debugging

--- IK_debug.py
+++ IK_debug.py
@@ -73,12 +73,12 @@ def test_code(test_case):
 
     # Modified DH param
     DH_Table = { alpha0:      0, a0:      0, d1:  0.75, q1:          q1,
-                 aplha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2. + q2,
+                 alpha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2. + q2,
                  alpha2:      0, a2:   1.25, d3:     0, q3:          q3,
                  alpha3: -pi/2., a3: -0.054, d4:   1.5, q4:          q4,
                  alpha4:  pi/2., a4:      0, d5:     0, q5:          q5,
                  alpha5: -pi/2., a5:      0, d6:     0, q6:          q6,
-                 alpha6:      0, a6:      0, d7: 0.303, q7:          q7}
+                 alpha6:      0, a6:      0, d7: 0.303, q7:          0}
 
     # Define Modified DH Transformation matrix
     def TF_Matrix(alpha, a, d, q):
@@ -88,6 +88,7 @@ def test_code(test_case):
             [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d ],
             [                 0,                 0,           0,              1 ]
         ])
+	return TF
 
     # Create individual transformation matrices
     T0_1  = TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
@@ -98,7 +99,7 @@ def test_code(test_case):
     T5_6  = TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
     T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
 
-    T0_EE = T0_1 + T1_2 + T2_3 + T3_4 + T4_5 + T5_6 + T6_EE
+    T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
 
     # Extract end-effector position and orientation from request
     # px, py, pz = end-effector position
@@ -107,9 +108,9 @@ def test_code(test_case):
     py = req.poses[x].position.y
     pz = req.poses[x].position.z
 
-    (roll, pitch, yaw = tf.transformations.euler_from_quaternion(
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
         [req.poses[x].orientation.x, req.poses[x].orientation.y,
-            req.poses[x].orientation.z, req.poses[x].orientation.w]))
+            req.poses[x].orientation.z, req.poses[x].orientation.w])
 
     # Find EE rotation Matrix
     # Define RPY rotation matrices
@@ -125,9 +126,9 @@ def test_code(test_case):
                     [       0, 1,      0 ],
                     [ -sin(p), 0, cos(p) ]]) # pitch
 
-    ROT_z = Matrix([[ cos(y), -sin(y), 0) ],
+    ROT_z = Matrix([[ cos(y), -sin(y), 0 ],
                     [ sin(y),  cos(y), 0 ],
-                    [ 0,            0, 1 ]])
+                    [ 0,            0, 1 ]]) # YAW
 
     ROT_EE = ROT_z * ROT_y * ROT_x
 
@@ -140,21 +141,21 @@ def test_code(test_case):
                  [py],
                  [pz]])
 
-    WC = EE - 0.303 * ROT_EE[:,2]
+    WC = EE - (0.303) * ROT_EE[:,2]
 
     # Calculate joint angles using Geometric IK method
-    thera1 = atan2(WC[1], WC[0])
+    theta1 = atan2(WC[1], WC[0])
 
     # SSS triangle for theta2 and theta3
     side_a = 1.501
-    side_b = sqrt(pow((WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2)
+    side_b = sqrt(pow((sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
     side_c = 1.25
 
     angle_a = acos((side_b*side_b + side_c*side_c - side_a*side_a) / (2 * side_b * side_c))
     angle_b = acos((side_a*side_a + side_c*side_c - side_b*side_b) / (2 * side_a * side_c))
     angle_c = acos((side_a*side_a + side_b*side_b - side_c*side_c) / (2 * side_a * side_b))
 
-    theta2 = pi / 2 - angle_a -atan(WC[2] - 0.75, sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35)
+    theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35)
     theta3 = pi / 2 - (angle_b + 0.036) # 0.036 accounts for sag in link4 of -0.054m
 
     R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
@@ -163,9 +164,9 @@ def test_code(test_case):
     R3_6 = R0_3.inv("LU") * ROT_EE
 
     # Euler angles from rotation Matrix
-    theta4 = atan(R3_6[2,2], -R3_6[0,2])
-    theta5 = atan(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]), R3_6[1,2])
-    theta6 = atan(-R3_6[1,1], R3_6[1,0])
+    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+    theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]), R3_6[1,2])
+    theta6 = atan2(-R3_6[1,1], R3_6[1,0])
 
     ##
     ########################################################################################
@@ -175,7 +176,7 @@ def test_code(test_case):
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
-    FK = T0_EE-evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+    FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
 
     ## End your code input for forward kinematics here!
     ########################################################################################
